32641eb68230b09595826f7d21b6f5c051f23501
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:54:19 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio11/Program.cs
@@ -0,0 +1,36 @@
+﻿//Crea un programa, que pida un número del 1 al 7,  devuelva el día de la semana , teniendo en cuenta que el 1 es Lunes, hacer con un switch.
+Console.WriteLine("Ingrese un número del 1 al 7:");
+int numero123 = Convert.ToInt32(Console.ReadLine());
+if (numero123 % 2 == 0)
+{
+    switch (numero123)
+    {
+        case 2:
+            Console.WriteLine("Martes");
+            break;
+        case 4:
+            Console.WriteLine("Jueves");
+            break;
+        case 6:
+            Console.WriteLine("Sábado");
+            break;
+    }
+}
+else
+{
+    switch (numero123)
+    {
+        case 1:
+            Console.WriteLine("Lunes");
+            break;
+        case 3:
+            Console.WriteLine("Miércoles");
+            break;
+        case 5:
+            Console.WriteLine("Viernes");
+            break;
+        case 7:
+            Console.WriteLine("Domingo");
+            break;
+    }
+}
